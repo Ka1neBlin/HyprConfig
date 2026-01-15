6d1cceb1a2b0ef95c497030ed093c2dfa304a081
Liza Belonina <fkme.fuhua@gmail.com>
2026-01-15 17:05:23 +0400

    Исправила скрипт

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 cp -r {hypr,kitty,mako,nvim,rofi,waybar} ~/.config
 cp -r simple-sddm /usr/share/sddm/themes
 cp .zshrc ~
+mkdir -p ~/Pictures/Background
+cp background.png ~/Pictures/Background
 
 sudo pacman -S mako nvim rofi waybar swaybg amberol nautilus hyprshot obsidian
