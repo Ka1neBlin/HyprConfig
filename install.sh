38ce00641fc3676ec09c13e668fe9c1ae1c59bef
Liza Belonina <fkme.fuhua@gmail.com>
2026-01-15 16:58:58 +0400

    Добавила скрипт установки

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp -r {hypr,kitty,mako,nvim,rofi,waybar} ~/.config
+cp -r simple-sddm /usr/share/sddm/themes
+cp .zshrc ~
+
+sudo pacman -S mako nvim rofi waybar swaybg amberol nautilus hyprshot obsidian
